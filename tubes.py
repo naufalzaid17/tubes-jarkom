65730c9e81e284f1792c8f8f00d1839b93656912
naufalzaid17 <naufalzaid17@gmail.com>
2022-06-16 19:31:18 +0700

    CLO1_fix

--- tubes.py
+++ tubes.py
@@ -17,24 +17,26 @@ p = Popen("sysctl -w %s=%s" %(key,value), shell=True, stdout=PIPE, stderr=PIPE)
 stdout, stderr = p.communicate()
 print("stdout=",stdout,"stderr=",stderr)
 
+# CLO 1 //
+
 # definisi variable untuk bandwidth
 bandwidth1={'bw':1} # bandwidth 1 mbps
 bandwidth2={'bw':0.5} # bandwidth 500kbps
 
 # definisi variable untuk router dan host
-hA = net.addHost('host_A')
-hB = net.addHost('host_B')
+host_A = net.addHost('host_A')
+host_B = net.addHost('host_B')
 R1 = net.addHost('R1')
 R2 = net.addHost('R2')
 R3 = net.addHost('R3')
 R4 = net.addHost('R4')
 
 # koneksi antar device
-net.addLink(hA,R2,cls=TCLink, **bandwidth1) #host_A-eth1 R2-eth0
-net.addLink(hA,R1,cls=TCLink, **bandwidth1) #host_A-eth0 R1-eth0
+net.addLink(host_A,R1,cls=TCLink, **bandwidth1) #host_A-eth0 R1-eth0
+net.addLink(host_A,R2,cls=TCLink, **bandwidth1) #host_A-eth1 R2-eth0
 
-net.addLink(hB,R3,cls=TCLink, **bandwidth1) #host_B-eth0 R3-eth0
-net.addLink(hB,R4,cls=TCLink, **bandwidth1) #host_B-eth1 R4-eth0
+net.addLink(host_B,R3,cls=TCLink, **bandwidth1) #host_B-eth0 R3-eth0
+net.addLink(host_B,R4,cls=TCLink, **bandwidth1) #host_B-eth1 R4-eth0
 
 net.addLink(R1,R3,cls=TCLink, **bandwidth2) #R1-eth1 R3-eth1
 net.addLink(R1,R4,cls=TCLink, **bandwidth1) #R1-eth2 R4-eth1
@@ -45,15 +47,15 @@ net.addLink(R2,R4,cls=TCLink, **bandwidth2) #R2-eth2 R4-eth2
 net.build()
 
 # konfiguari host 
-hA.cmd("ifconfig hA-eth0 0")
-hA.cmd("ifconfig hA-eth1 0")
-hA.cmd("ifconfig hA-eth0 192.168.42.1 netmask 255.255.255.0")
-hA.cmd("ifconfig hA-eth1 192.168.52.1 netmask 255.255.255.0")
+host_A.cmd("ifconfig host_A-eth0 0")
+host_A.cmd("ifconfig host_A-eth1 0")
+host_A.cmd("ifconfig host_A-eth0 192.168.42.1 netmask 255.255.255.0")
+host_A.cmd("ifconfig host_A-eth1 192.168.52.1 netmask 255.255.255.0")
 
-hB.cmd("ifconfig hA-eth0 0")
-hB.cmd("ifconfig hA-eth1 0")
-hB.cmd("ifconfig hA-eth0 192.168.62.1 netmask 255.255.255.0")
-hB.cmd("ifconfig hA-eth1 192.168.72.1 netmask 255.255.255.0")
+host_B.cmd("ifconfig host_B-eth0 0")
+host_B.cmd("ifconfig host_B-eth1 0")
+host_B.cmd("ifconfig host_B-eth0 192.168.62.1 netmask 255.255.255.0")
+host_B.cmd("ifconfig host_B-eth1 192.168.72.1 netmask 255.255.255.0")
 
 # konfigurasi router
 R1.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
@@ -88,6 +90,12 @@ R4.cmd("ifconfig R4-eth2 0")
 R4.cmd("ifconfig R4-eth0 192.168.72.2 netmask 255.255.255.0")
 R4.cmd("ifconfig R4-eth1 192.168.102.2 netmask 255.255.255.0")
 R4.cmd("ifconfig R4-eth2 192.168.92.2 netmask 255.255.255.0")
+
+# // CLO 1
+
+# CLO 2 //
+
+# 
 
 CLI(net)
 net.stop()
